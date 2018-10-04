f8ac39991a779a485895ce8e70f22482f4bc9a14
SimonBerens <sberensnyc@gmail.com>
2018-10-03 21:04:25 -0400

    json stringify user and modulize key pair bit and transaction minimum value

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 DIFFICULTY = 2
-ME = {'alias': 'Simon', 'hashed_id': '0'}
+ME = '{"alias": "Simon", "hashed_id": "0", "public_key": "None", "private_key": "None"}'
+TRANSACTION_MIN = 0
+NUM_KEY_BITS = 2048

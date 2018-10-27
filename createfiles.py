3cce0d2c616af857f0afa9310fe9e6cf4386bb43
SimonBerens <sberensnyc@gmail.com>
2018-10-26 21:45:44 -0400

    Store hashed and unhashed id in me.json

--- createfiles.py
+++ createfiles.py
@@ -3,6 +3,7 @@ from json import dumps
 with open("me.json", "w+") as f:
     temp = dict()
     temp['alias'] = 'Anon'
+    temp['hashed_id'] = 'Temp'
     temp['unhashed_id'] = 'Temp'
     k = RSA.generate(2048)
     temp['public_key'] = k.publickey().export_key().decode()

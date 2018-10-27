78d676d198f481114f8074c5ae64afdf9b42030f
SimonBerens <sberensnyc@gmail.com>
2018-10-26 21:41:12 -0400

    Allow for easier key generation

---
+++ createfiles.py
@@ -0,0 +1,16 @@
+from Cryptodome.PublicKey import RSA
+from json import dumps
+with open("me.json", "w+") as f:
+    temp = dict()
+    temp['alias'] = 'Anon'
+    temp['unhashed_id'] = 'Temp'
+    k = RSA.generate(2048)
+    temp['public_key'] = k.publickey().export_key().decode()
+    temp['private_key'] = k.export_key().decode()
+    f.write(dumps(temp))
+
+with open("users.json", "w+") as f:
+    f.write("[]")
+
+with open("nodes.json", "w+") as f:
+    f.write("[]")

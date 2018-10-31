2f3e3b0dd17a325ac5e02b4fd29d698aa30edd03
SimonBerens <sberensnyc@gmail.com>
2018-10-31 14:42:16 -0400

    Create me.json from user input

--- createfiles.py
+++ createfiles.py
@@ -2,9 +2,9 @@ from Cryptodome.PublicKey import RSA
 from json import dumps
 with open("me.json", "w+") as f:
     temp = dict()
-    temp['alias'] = 'Anon'
-    temp['hashed_id'] = 'Temp'
-    temp['unhashed_id'] = 'Temp'
+    temp['alias'] = input("Your alias: ")
+    temp['hashed_id'] = input("Your hashed id: ")
+    temp['unhashed_id'] = input("Your unhashed id: ")
     k = RSA.generate(2048)
     temp['public_key'] = k.publickey().export_key().decode()
     temp['private_key'] = k.export_key().decode()
